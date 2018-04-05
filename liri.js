fa4b3164fbee3bdf7a33a24b386b13f2ce6e4470
Adam Miner <miner.adam1@hotmail.com>
2018-04-05 13:27:51 -0600

    Initial Commit

---
+++ liri.js
@@ -0,0 +1,7 @@
+require('dotenv').config();
+var keys = require('./keys.js');
+var Spotify = require('node-spotify-api');
+var Twitter = require('twitter');
+var request = require('request');
+var spotify = new Spotify(keys.spotify);
+var client = new Twitter(keys.twitter);
