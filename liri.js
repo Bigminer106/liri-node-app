2b23adf2caa37d1ddc545a9c31870f55777d040f
Adam Miner <miner.adam1@hotmail.com>
2018-06-26 17:33:00 -0600

    Pseudocode Portions

--- liri.js
+++ liri.js
@@ -22,23 +22,24 @@ fs.appendFile('log.txt', result, 'utf8', (err) => {
   switch (command) {
     case 'my-tweets':
       const queryURL = `https://www.twitter.com/bigminer106/tweets&appid=${keys.twitter}&limit=20`;
-      
+      // Insert process.env statement to pull latest 20 tweets from Twitter API
       break;
 
     case 'spotify-this-song':
       const queryURL = `https://www.spotify.com/?q=${query}&appid=${keys.spotify}`;
-      
+      // Insert process.env statement to pull the song from Spotify API
       break;
 
     case 'movie-this':
       const queryURL = `https://www.omdbapi.com?q=${query}&appid=trilogy`;
-      
+      // Search OMDB API for the right movie
       break;
 
     case 'do-what-it-says':
       fs.readFile('random.txt', 'utf8', err => {
         if (err) throw err;
       });
+      // Insert process.env statement for LIRI to read the file
       break;
   
     default:
