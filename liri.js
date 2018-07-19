34d47b735cfa7601fd7ebea1469f55851157e76a
Adam Miner <miner.adam1@hotmail.com>
2018-07-19 09:56:53 -0600

    Tweaks

--- liri.js
+++ liri.js
@@ -9,84 +9,75 @@ const request = require('request');
 // API Keys
 const spotify = new Spotify(keys.spotify);
 const client = new Twitter(keys.twitter);
-const result = '';
 
-const obj = process.env;
-const arr = Object.keys(obj).map(key => {
-  return obj[key];
+// Convert process.env object to array
+const arr = Object.keys(process.env).map(key => {
+  return process.env[key];
 });
 
 // command & query constants
-const command = arr[2];
+const command = process.env[2];
 const query = arr.slice(3);
 
-fs.appendFile('log.txt', result, 'utf8', (err) => {
-  if (err) {
-    throw err;
-  } else if (command === 'my-tweets') {
-    console.log('Oi, Twitter...');
-    let urlTweet = `https://www.twitter.com/bigminer106/tweets&appid=${client}&limit=20`;
+// Result constant
+const result = '';
+
+// These console.logs work, but not the way I thought they did.
+// console.log(spotify);
+// console.log(client);
+
+if (command === 'my-tweets') {
+  console.log('Oi, Twitter...'); // Sanity Check; not working
+  let urlTweet = `https://www.twitter.com/bigminer106/tweets&appid=${keys.twitter}&limit=20`;
+  
+  request(urlTweet, (err, res) => {
+    if (err) {
+      throw err;
+    } else if (!err && res.statusCode === 200) {
+      fs.appendFile('log.txt', client, 'utf8', (err) => {
+        if (err) throw err;
+        console.log('Written!');
+      });
+    }
+  });
+} else if (command === 'spotify-this-song') {
+  console.log('How about some tunes?'); // Sanity Check; not working
+  let urlSpot = `https://www.spotify.com/?q=${query}&appid=${keys.spotify}`;
+  
+  request(urlSpot, (err, res) => {
+    if (err) {
+      throw err;
+    } else if (!err && res.statusCode === 200) {
+      fs.appendFile('log.txt', spotify.data, 'utf8', (err) => {
+        if (err) throw err;
+        console.log('Written!');
+      });
+    }
+  });
+} else if (command === 'movie-this') {
+  console.log('Movie Night!'); // Sanity Check; not working
+  let urlOMDB = `https://www.omdbapi.com?q=${query}&appid=trilogy`;
     
-    request(urlTweet, (err, res, body) => {
-      if (err) {
-        throw err;
-      } else if (!err && res.statusCode === 200) {
-        result = JSON.stringify(body, null, 2);
-        fs.writeFile('./log.txt', result, 'utf8', (err) => {
-          if (err) {
-            throw err;
-          };
-          console.log('Written!');
-        });
-      }
-    });
-  } else if (command === 'spotify-this-song') {
-    console.log('How about some tunes?');
-    let urlSpot = `https://www.spotify.com/?q=${query}&appid=${spotify}`;
-    
-    request(urlSpot, (err, res, body) => {
-      if (err) {
-        throw err;
-      } else if (!err && res.statusCode === 200) {
-        result = JSON.stringify(body, null, 2);
-        fs.writeFile('./log.txt', result, 'utf8', (err) => {
-          if (err) {
-            throw err;
-          };
-          console.log('Written!');
-        });
-      }
-    });
-  } else if (command === 'movie-this') {
-    console.log('Movie Night!');
-    let urlOMDB = `https://www.omdbapi.com?q=${query}&appid=trilogy`;
-      
-    request(urlOMDB, (err, res, body) => {
-      if (err) {
-        throw err;
-      } else if (!err && res.statusCode === 200) {
-        result = JSON.stringify(body, null, 2);
-        fs.writeFile('./log.txt', result, 'utf8', (err) => {
-          if (err) {
-            throw err;
-          };
-          console.log('Written!');
-        });
-      }
-    })
-  } else if (command === 'do-what-it-says') {
-    console.log('Doing it...');
-    fs.readFile('./assets/random.txt', 'utf8', err => {
-      if (err) {
-        throw err;
-      } else {
-        fs.writeFile('./log.txt', result, 'utf8', (err) => {
-          if (err) {
-            throw err;
-          }
-          console.log('Written!');
-        });
-      }
-    });
-  };
-});
+  request(urlOMDB, (err, res) => {
+    if (err) {
+      throw err;
+    } else if (!err && res.statusCode === 200) {
+      fs.appendFile('log.txt', result, 'utf8', (err) => {
+        if (err) throw err;
+        console.log('Written!');
+      });
+    }
+  })
+} else if (command === 'do-what-it-says') {
+  console.log('Doing it...'); // Sanity Check; not working
+  fs.readFile('./assets/random.txt', 'utf8', err => {
+    if (err) {
+      throw err;
+    } else {
+      fs.appendFile('log.txt', result, 'utf8', (err) => {
+        if (err) throw err;
+        console.log('Written!');
+      });
+    }
+  });
+};
